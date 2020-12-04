8d65dbb159eff887cff9b197a2c18f1c55a51ffc
Yoann <yoannpascalbouix@gmail.com>
2020-12-04 09:12:19 +0100

    Edit todo by index; Add todo on enter

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ function enregistrerValeur () {
     const valeur = $('.title').val()
     todoList.push(valeur)
     clearInput ()
-    afficherValeur()
-    
+    afficherValeur()  
 }
 
 function clearInput () {
@@ -13,46 +12,37 @@ function clearInput () {
 }
 
 function afficherValeur (arg = null) {
-    console.log("entre afficher "+arg)
-    
     $('#list > li').remove();
     for (i = 0; i < todoList.length; i++){
-        if (todoList[i] === arg) {
-            $('#list').append(`<li><input type="text" class="input-edit" value="${todoList[i]}">
-            <button type="button" onclick="editText(` + i +`)">Edit</button></li>`)
+        if (i === arg) {
+            $('#list').append(
+                `<li><input type="text" class="input-edit" value="${todoList[i]}">
+                <button type="button" onclick="editText(${i})">Edit</button></li>`)
         } 
         else {
-            $('#list').append(`<li>${todoList[i]} <a class="edit" href="#" onclick="effacerToDo(` + i +`)">X</a><a class="edit" href="#" onclick="afficherValeur('${todoList[i]}')"> E</a></li>`)
+            $('#list').append(
+                `<li>${todoList[i]} 
+                <a class="edit" href="#" onclick="effacerToDo(${i})">X</a>
+                <a class="edit" href="#" onclick="afficherValeur(${i})"> E</a></li>`)
         }
     }
 }
-
-
 
 function effacerToDo (arg) {
     console.log('arg effacer' + arg)
     todoList.splice(arg, 1)
     afficherValeur()
-    
 }
 
 function editText (arg) {
     const editedToDo = $('.input-edit').val()
-        todoList.splice(arg, 1, editedToDo)
-        afficherValeur()
+    todoList.splice(arg, 1, editedToDo)
+    afficherValeur()
 }
 
-// function editText (arg) {
-//     const editedToDo = $('.input-edit').val()
-//     const listToEdit = todoList.findIndex(element => element === arg)
-//     todoList.splice(listToEdit, 1, editedToDo)
-//     afficherValeur()
-// }
-
-var input = $('.title')
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        $("#button").click();
+function pressEnter (e) {
+    if (e.keyCode === 13) {
+        enregistrerValeur()
     }
-})
+}
+   
